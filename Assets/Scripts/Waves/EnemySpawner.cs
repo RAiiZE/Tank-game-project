0983d920ad016a48a6f5e9934084d00ceb3f757b
AIE\s215684 <chaoticlive127@hotmail.com>
2022-09-14 20:22:18 +0930

    added notes, random spawner

--- Assets/Scripts/Waves/EnemySpawner.cs
+++ Assets/Scripts/Waves/EnemySpawner.cs
@@ -9,9 +9,24 @@ public class EnemySpawner : MonoBehaviour
 
     public int wave = 0;
 
+    public Transform spawnPointsParent;
+    List<Vector3> spawnPoints = new List<Vector3> ();
+
     public GameManager gameManager;
+
+    private void Awake()
+    {
+        // loop to run through each potential spawn point
+        foreach (Transform spawnPoint in spawnPointsParent)
+        {
+            spawnPoints.Add(spawnPoint.position);
+        }
+    }
+
+
     public void NextWave()
     {
+        // on next wave, wave goes up by 1
         wave += 1;
 
         for (int i = 1; i < gameManager.m_Tanks.Length; i++)
@@ -21,14 +36,18 @@ public class EnemySpawner : MonoBehaviour
 
         gameManager.m_Tanks = new GameObject[wave + 1];
         gameManager.m_Tanks[0] = playerTank;
-
+        
+        
         playerTank.SetActive(true);
+
+        // on new wave, check how many tanks, if its less than the number of the wave, spawn 1 more at the potential spawn points.
         for (int i = 1; i <= wave; i++)
         {
-            gameManager.m_Tanks[i] = Instantiate(enemyPrefab);
+            gameManager.m_Tanks[i] = Instantiate(enemyPrefab, spawnPoints[Random.Range(0, spawnPoints.Count)], Quaternion.identity);
         }
     }
 
+    // on death, wave becomes 0
     public void PlayerDeath()
     {
         wave = 0;
